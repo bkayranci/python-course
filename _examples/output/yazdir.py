b7a64234bf542c4f5a605e535a17154b52c5a303
Türkalp Burak KAYRANCIOĞLU <bkayranci@gmail.com>
2018-10-11 15:04:16 +0300

    dev

---
+++ _examples/output/yazdir.py
@@ -0,0 +1,13 @@
+print(5)
+
+print(5.34)
+
+print("sample text")
+print('sample text')
+
+print([1,2,3])
+
+print((1,2,3))
+
+print("Ürün adı:", "Kahve")
+print("Ürün adedi:", 5, "Birim fiyatı:", 1)
